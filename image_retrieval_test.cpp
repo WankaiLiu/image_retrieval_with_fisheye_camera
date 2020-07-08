9db0648f06708b1bbb58579463c24a3930dacd04
LiuWankai <liuwk1@lenovo.com>
2020-07-08 14:29:36 +0800

    handle file list for our dataset

--- image_retrieval_test.cpp
+++ image_retrieval_test.cpp
@@ -8,7 +8,7 @@
 void LoadImages(const string &strImagePath, const string &strTimesStampsPath,
                 vector<string> &strImagesFileNames)
 {
-    std::cout << "LoadImages(): " << strTimesStampsPath << std::endl;
+    if(DEBUG_INFO) std::cout << "LoadImages(): " << strTimesStampsPath << std::endl;
     ifstream fTimes;
     fTimes.open(strTimesStampsPath.c_str());
     strImagesFileNames.reserve(5000);
@@ -35,37 +35,63 @@ void LoadImages(const string &strImagePath, const string &strTimesStampsPath,
     fTimes.close();
 }
 
+void LoadPathList( const string &fileListsPath, vector<string> &fileVec)
+{
+    if(DEBUG_INFO) std::cout << "LoadImages(): " << fileListsPath << std::endl;
+    ifstream fTimes;
+    fTimes.open(fileListsPath.c_str());
+    while (!fTimes.eof())
+    {
+        string s;
+        getline(fTimes, s);
+        if (!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            if(DEBUG_INFO) std::cout << "load set: " << ss.str() << std::endl;
+            fileVec.push_back(ss.str());
+        }
+    }
+    fTimes.close();
+}
 
 
 int main()
 {
-
-    std::string voc_path= "/home/wankai/project/image_retrieval/config/loopC_vocdata.bin";
-
-
-    string base_path = "/home/wankai/data/slam/slamdata-Chicken/slamdata-circle-01/";
-    string image_path = base_path + "cam0";
-    string timeStamps = base_path + "loop.txt";
+    string voc_path= "/home/wankai/project/image_retrieval/config/loopC_vocdata.bin";
+    string fileListsPath = "../config/datalist1.txt";
     std::string pattern_file = "/home/wankai/project/image_retrieval/config/loopC_pattern.yml";
     ImageDatabase imdb(voc_path, pattern_file);
-    vector<string> imagesList;
-//    ./estimator_test -c=/home/wankai/data/slam/slamdata-Chicken/sunflower/config_g2.yaml -l=$DATA_DIR/$DATA/cam0
-//            -r=$DATA_DIR/$DATA/cam1 -ts=$DATA_DIR/$DATA/loop.txt -imu=$DATA_DIR/$DATA/imu0.csv
-    if(!image_path.empty()) {
-
-        LoadImages(image_path, timeStamps, imagesList);
-        cout << "The size of image list is " << imagesList.size() << endl;
+    vector<string> fileVec;
+    LoadPathList(fileListsPath, fileVec);
+    for(auto i = 0; i < fileVec.size(); i++) {
+        string base_path = fileVec[i];
+        string image_path = base_path + "/cam0";
+        string timeStamps = base_path + "/loop.txt";
+        vector<string> imagesList;
+        if (!image_path.empty()) {
+            LoadImages(image_path, timeStamps, imagesList);
+            cout << "The size of image list is " << imagesList.size() << endl;
+        }
+        for (int ni = 0; ni < 10; ni++) {
+            cv::Mat image = cv::imread(imagesList[ni], CV_LOAD_IMAGE_UNCHANGED);
+            imdb.addImage(image, i);
+        }
     }
-//    for (int ni = 0; ni < imagesList.size(); ni++) {
-    for (int ni = 0; ni < 10; ni++) {
-        cv::Mat image = cv::imread(imagesList[ni], CV_LOAD_IMAGE_UNCHANGED);
-        imdb.addImage(image, ni%3);
+    for(auto i = 0; i < fileVec.size(); i++) {
+        string base_path = fileVec[i];
+        string image_path = base_path + "/cam0";
+        string timeStamps = base_path + "/loop.txt";
+        vector<string> imagesList;
+        if (!image_path.empty()) {
+            LoadImages(image_path, timeStamps, imagesList);
+            cout << "The size of image list is " << imagesList.size() << endl;
+        }
+        for (int ni = 0; ni < imagesList.size(); ni++) {
+            cv::Mat image = cv::imread(imagesList[ni], CV_LOAD_IMAGE_UNCHANGED);
+            int id = imdb.query(image);
+            cout << "The result is " << id << endl;
+        }
     }
-    for (int ni = 0; ni < imagesList.size(); ni++) {
-        cv::Mat image = cv::imread(imagesList[ni], CV_LOAD_IMAGE_UNCHANGED);
-        int id = imdb.query(image);
-        cout << "The result is " << id << endl;
-    }
-//    BriefVocabulary* voc = new BriefVocabulary();
 
 }
