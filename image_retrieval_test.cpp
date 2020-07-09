850a960a7e4d5dc698d4fd4f7e8534af61126e32
LiuWankai <liuwk1@lenovo.com>
2020-07-09 15:16:11 +0800

    add tic_toc to eval process time

--- image_retrieval_test.cpp
+++ image_retrieval_test.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <iostream>
 //using namespace std;
-//#include "brief.h"
+#include "tic_toc.h"
 #include "ImageDatabase.h"
 
 
@@ -59,12 +59,17 @@ void LoadPathList( const string &fileListsPath, vector<string> &fileVec)
 
 int main()
 {
+    int addStep = 10;
+    int queryStep = 10;
     string voc_path= "/home/wankai/project/image_retrieval/config/loopC_vocdata.bin";
-    string fileListsPath = "../config/datalist1.txt";
+    string fileListsPath = "../config/datalist2.txt";
     std::string pattern_file = "/home/wankai/project/image_retrieval/config/loopC_pattern.yml";
     ImageDatabase imdb(voc_path, pattern_file);
     vector<string> fileVec;
     LoadPathList(fileListsPath, fileVec);
+    TicToc t_loadImage;
+    double loadImageTimeCost, queryImageTimeCost;
+    int counterDb = 0, counterQuery = 0;
     for(auto i = 0; i < fileVec.size(); i++) {
         string base_path = fileVec[i];
         string image_path = base_path + "/cam0";
@@ -74,14 +79,17 @@ int main()
             LoadImages(image_path, timeStamps, imagesList);
             cout << "The size of image list is " << imagesList.size() << endl;
         }
-        for (int ni = 0; ni < imagesList.size(); ni += 20) {
+        for (int ni = 0; ni < imagesList.size(); ni += addStep) {
             cv::Mat image = cv::imread(imagesList[ni], CV_LOAD_IMAGE_UNCHANGED);
             imdb.addImage(image, i);
         }
+        counterDb += imagesList.size() / addStep;
     }
-    string fileListsPathQuery = "../config/datalist3.txt";
+    loadImageTimeCost = t_loadImage.toc();
+    string fileListsPathQuery = "../config/datalist1.txt";
     LoadPathList(fileListsPathQuery, fileVec);
     vector<int> counter1,counter2,counter3;
+    TicToc t_queryImage;
     for(auto i = 0; i < fileVec.size(); i++) {
         counter1.push_back(0);
         counter2.push_back(0);
@@ -94,7 +102,7 @@ int main()
             LoadImages(image_path, timeStamps, imagesList);
             cout << "The size of image list is " << imagesList.size() << endl;
         }
-        for (int ni = 0; ni < imagesList.size(); ni += 20) {
+        for (int ni = 0; ni < imagesList.size(); ni += queryStep) {
             cv::Mat image = cv::imread(imagesList[ni], CV_LOAD_IMAGE_UNCHANGED);
             int id = imdb.query(image);
             counter1[i]++;
@@ -102,11 +110,20 @@ int main()
             if(id == -1) counter3[i]++;
             cout << "The setid and result is: " << i << " - " << id << endl;
         }
+        counterQuery += imagesList.size() / queryStep;
     }
+    queryImageTimeCost = t_loadImage.toc();
     for(auto i = 0; i < fileVec.size(); i++) {
         cout << "set id: " << i << endl;
         cout << "The match number is: " << counter2[i] << endl;
         cout << "The failed number is: " << counter3[i] << endl;
         cout << "The total number is: " << counter1[i] << endl;
+        cout << "Success rate is: " << 1.0f * counter2[i] / counter1[i] << endl;
     }
+    cout << "loadImageTimeCost(s) is: " << loadImageTimeCost / 1000 << endl;
+    cout << "queryImageTimeCost(s) is: " << queryImageTimeCost / 1000 << endl;
+    cout << "loadImageTotalNumber is: " << counterDb << endl;
+    cout << "queryImageTotalNumber is: " << counterQuery << endl;
+    cout << "avg loadImageTimeCost(ms) is: " << loadImageTimeCost / counterDb<< endl;
+    cout << "avg queryImageTimeCost(ms) is: " << queryImageTimeCost / counterQuery << endl;
 }
